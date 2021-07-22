cf4cb2d53ea9c6e39358bac8e077bd4ce8dd4d42
Prasanna-Neelam <prasannaneelam15@gmail.com>
2021-07-22 07:49:35 +0530

    Delivery Service implemented

---
+++ Whileloop.py
@@ -0,0 +1,13 @@
+# This program ships packages from one room to another
+
+n = int(input("How many packages are there: "))
+while n > 0:
+    print(str(n) + " Package collected ⌻")
+    print(' ')
+    print(" . . . . . . . . . ")
+    print(' ')
+    print("Package Delivered ☻ ")
+    print(' ')
+    n = n - 1
+    print(' ')
+print("All packages moved successfully ☃ ")
