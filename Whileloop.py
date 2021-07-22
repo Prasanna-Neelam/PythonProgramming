438b731cceb1f813cc0baaa1909ced2cfc115d39
Prasanna-Neelam <prasannaneelam15@gmail.com>
2021-07-22 08:21:02 +0530

    corrected the sentence formatting

--- Whileloop.py
+++ Whileloop.py
@@ -2,7 +2,7 @@
 
 n = int(input("How many packages are there: "))
 while n > 0:
-    print(str(n) + " Package collected ⌻")
+    print("Package no: " + str(n) + " collected ⌻")
     print(' ')
     print(" . . . . . . . . . ")
     print(' ')
